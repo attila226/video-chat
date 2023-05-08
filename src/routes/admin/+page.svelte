5894c2ffec4128795d013684f0996f5709d33080
attila226 <attilabuturla@gmail.com>
2023-05-08 10:40:03 -0700

    Add basic styling to admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -10,12 +10,32 @@
 	const db = initDB(firebaseConfig);
 </script>
 
-<div>
-	<span>
+<header class="header">
+	<a href="/">Home</a>
+	<a href="/admin">Admin</a>
+</header>
+<main>
+	<span class="content">
 		<button
 			class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
 			on:click={async () => await deleteAllCallls(db)}
 			>Remove all Calls
 		</button>
 	</span>
-</div>
+</main>
+
+<style>
+	.header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	a {
+		padding: 1rem;
+		font-size: 1rem;
+	}
+	.content {
+		display: flex;
+		justify-content: center;
+	}
+</style>
